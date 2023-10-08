54f9f9039d718c173008070eec98d62f2ceb94da
Sunonline <32479379+SamuiSmartHome@users.noreply.github.com>
2023-10-08 11:47:23 +0700

    Update app_refactored.py

--- app_refactored.py
+++ app_refactored.py
@@ -35,7 +35,7 @@ col1, col2, col3 = st.columns(3)
 for i in range(len(crpytoList.keys())):
    selected_crypto_label = list(crpytoList.keys())[i]
    selected_crypto_index = list(df.symbol).index(crpytoList[selected_crypto_label])
-    selected_crypto = st.sidebar.selectbox(selected_crypto_label, df.symbol, selected_crypto_index, key = str(i))
+   selected_crypto = st.sidebar.selectbox(selected_crypto_label, df.symbol, selected_crypto_index, key = str(i))
     col_df = df[df.symbol == selected_crypto]
     col_price = round_value(col_df.lastPrice)
     col_percent = f'{float(col_df.priceChangePercent)}%'
